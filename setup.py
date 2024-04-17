6dc96cde0fa43658bb69ec6cc3ac83b9e04efcf5
Mukuk88 <mukukkchaudhary@gmail.com>
2024-04-16 23:22:12 -0700

    file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'mcqgenerator',
+    version='0.0.1',
+    author='Mukul Chaudhary',
+    author_email='mukukkchaudhary@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+
+)
